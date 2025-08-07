729d8508739bd2bc60a07de2b109e8820f410097
hicd07 <hicd07@users.noreply.github.com>
2025-08-06 20:00:42 -0400

    Fix React hook compatibility by adding react-native-web alias

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,7 @@ module.exports = function (api) {
           root: ['./'],
           alias: {
             '@': './',
+            'react-native': 'react-native-web',
           },
         },
       ],
